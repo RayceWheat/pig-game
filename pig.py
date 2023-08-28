47d8cadc49b7fa8451a1315af092267f532d73ea
Rayce <RayceWheat@Gmail.com>
2023-08-28 18:07:36 -0500

    impalmented roll() function

---
+++ pig.py
@@ -0,0 +1,30 @@
+#allow user to roll the dice rng (1-6)
+#Roll again or stop
+#add total score
+#check if score above 50
+#decalre iwnner
+
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("Invalid, try again.")    
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+print(player_scores)
